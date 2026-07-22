207cdeff3b662f60f7d55a48f5660b6c78fe9cad
technbuzz <samiullah.khan.1989@gmail.com>
2026-07-23 00:43:44 +0400

    refactor(table

--- ng/table-sort/src/app/app.ts
+++ ng/table-sort/src/app/app.ts
@@ -1,16 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { SlicePipe } from '@angular/common';
+import { httpResource } from '@angular/common/http';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+type Comment = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Hello, {{ title() }}</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet, SlicePipe],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('table-sort');
+  protected readonly title = signal('table-sort-basic');
+
+  comments = httpResource<Comment[]>(() => ({
+    url: 'https://jsonplaceholder.typicode.com/comments',
+    params: {
+      _limit: 10
+    }
+  }), {defaultValue: []},
+  );
+
 }

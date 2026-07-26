2f984ca1ef304cf6f2c10e1ae214f8ceab165811
technbuzz <samiullah.khan.1989@gmail.com>
2026-07-26 23:54:27 +0400

    feat(table-sort): added httpResoure snapshot

--- ng/table-sort/src/app/app.ts
+++ ng/table-sort/src/app/app.ts
@@ -1,6 +1,6 @@
 import { SlicePipe } from '@angular/common';
 import { httpResource } from '@angular/common/http';
-import { Component, effect, signal } from '@angular/core';
+import { Component, effect, linkedSignal, Resource, resourceFromSnapshots, ResourceSnapshot, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 type Comment = {
@@ -9,6 +9,21 @@ type Comment = {
   name: string;
   email: string;
   body: string;
+}
+
+function withPreviousValue<T>(input: Resource<T>):Resource<T> {
+  
+  const derived = linkedSignal<ResourceSnapshot<T>, ResourceSnapshot<T>>({
+    source: input.snapshot,
+    computation: (snap, previous) => {
+      if (snap.status === 'loading' && previous && previous.value.status !== 'error') {
+        return { status: 'loading' as const, value: previous.value.value  }
+      }
+      return snap
+    }
+  })
+
+  return resourceFromSnapshots(derived) 
 }
 
 @Component({
@@ -20,12 +35,29 @@ type Comment = {
 export class App {
   protected readonly title = signal('table-sort-basic');
 
+  sort = signal('');
+  order = signal('asc');
+
   comments = httpResource<Comment[]>(() => ({
-    url: 'https://jsonplaceholder.typicode.com/comments',
-    params: {
-      _limit: 10
-    }
-  }), {defaultValue: []},
+      url: 'https://jsonplaceholder.typicode.com/comments',
+      params: {
+        _limit: 10,
+        _sort: this.sort(),
+        _order: this.order()
+      }
+    }), {defaultValue: []},
   );
 
+  // comments = withPreviousValue(this.commentsRes);
+
+  sortBy(field: string) {
+    if(this.sort() === field) {
+      const newDir = this.order() === 'asc' ? 'desc' : 'asc';
+      this.order.set(newDir)
+      return
+    }
+
+    this.sort.set(field);
+    this.order.set('asc');
+  }
 }
